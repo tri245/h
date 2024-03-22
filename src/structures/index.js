d89189ce4ef132aa775017a889bd424eadbb57e3
Tri_nho <76763671+tri245@users.noreply.github.com>
2024-03-22 15:01:45 +0700

    Delete src directory

--- src/structures/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	Guild: require('./Guild'),
-	User: require('./User'),
-	Message: require('./Message'),
-	TextChannel: require('./TextChannel'),
-	DMChannel: require('./DMChannel'),
-	VoiceChannel: require('./VoiceChannel'),
-	parseVideo: require('./YoutubeVideo').parseVideo,
-};
